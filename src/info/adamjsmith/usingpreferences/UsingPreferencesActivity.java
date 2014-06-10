3ad1fc420e6c5e3003a8bdb30aabd0e3bb62ff6a
Adam Smith <adam.smith18@ntlworld.com>
2014-06-10 10:52:06 +0100

    Added saving to a seperate file

--- src/info/adamjsmith/usingpreferences/UsingPreferencesActivity.java
+++ src/info/adamjsmith/usingpreferences/UsingPreferencesActivity.java
@@ -1,5 +1,11 @@
 package info.adamjsmith.usingpreferences;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -9,11 +15,16 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class UsingPreferencesActivity extends Activity {
+	
+	EditText textBox;
+	static final int READ_BLOCK_SIZE = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        textBox = (EditText) findViewById(R.id.txtText1);
     }
     
     public void onClickLoad(View view) {
@@ -35,6 +46,43 @@ public class UsingPreferencesActivity extends Activity {
     
     private void DisplayText(String str) {
     	Toast.makeText(getBaseContext(), str, Toast.LENGTH_LONG).show();
+    }
+    
+    public void onClickSaveFile(View view) {
+    	String str = textBox.getText().toString();
+    	try {
+    		FileOutputStream fOut = openFileOutput("textfile.txt", MODE_WORLD_READABLE);
+    		OutputStreamWriter osw = new OutputStreamWriter(fOut);
+    		
+    		osw.write(str);
+    		osw.flush();
+    		osw.close();
+    		
+    		DisplayText("File Saved Successfully!");
+    	} catch(IOException ioe) {
+    		ioe.printStackTrace();
+    	}
+    }
+    
+    public void onClickLoadFile(View view) {
+    	try {
+    		FileInputStream fIn = openFileInput("textfile.txt");
+    		InputStreamReader isr = new InputStreamReader(fIn);
+    		
+    		char[] inputBuffer = new char[READ_BLOCK_SIZE];
+    		String s = "";
+    		
+    		int charRead;
+    		while ((charRead = isr.read(inputBuffer))>0) {
+    			String readString = String.copyValueOf(inputBuffer, 0, charRead);
+    			s += readString;
+    			inputBuffer = new char[READ_BLOCK_SIZE];
+    		}
+    		textBox.setText(s);
+    		DisplayText("File Loaded successfully!");
+    	} catch (IOException ioe) {
+    		ioe.printStackTrace();
+    	}
     }
     
 }
