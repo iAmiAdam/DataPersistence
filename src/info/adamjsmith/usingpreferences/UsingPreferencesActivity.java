f512ad99cfccd7415f3a30b3e6e6929f45583ded
Adam Smith <adam.smith18@ntlworld.com>
2014-06-10 10:22:11 +0100

    Saving and fetching app preferences

--- src/info/adamjsmith/usingpreferences/UsingPreferencesActivity.java
+++ src/info/adamjsmith/usingpreferences/UsingPreferencesActivity.java
@@ -2,8 +2,11 @@ package info.adamjsmith.usingpreferences;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class UsingPreferencesActivity extends Activity {
 
@@ -16,6 +19,22 @@ public class UsingPreferencesActivity extends Activity {
     public void onClickLoad(View view) {
     	Intent i = new Intent("info.adamjsmith.AppPreferenceActivity");
     	startActivity(i);
+    }
+    
+    public void onClickDisplay(View view) {
+    	SharedPreferences appPrefs = getSharedPreferences("info.adamjsmith.usingpreferences_preferences", MODE_PRIVATE);
+    	DisplayText(appPrefs.getString("editTextPref", "test"));
+    }
+    
+    public void onClickModify(View view) {
+    	SharedPreferences appPrefs = getSharedPreferences("info.adamjsmith.usingpreferences_preferences", MODE_PRIVATE);
+    	SharedPreferences.Editor prefsEditor = appPrefs.edit();
+    	prefsEditor.putString("editTextPref", ((EditText) findViewById(R.id.txtString)).getText().toString());
+    	prefsEditor.commit();
+    }
+    
+    private void DisplayText(String str) {
+    	Toast.makeText(getBaseContext(), str, Toast.LENGTH_LONG).show();
     }
     
 }
